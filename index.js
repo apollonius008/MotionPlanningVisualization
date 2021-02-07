330a94bdbe3bb8a36d1dc43066e700d19f030449
Sarit Kumar Das <quantumalex008@gmail.com>
2021-02-07 10:59:43 +0530

    COMMENT TEST CODE

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ let board;
 function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent('#canvas-window');
-    boardTestInit();
     
     window.onresize = () => {
         resizeCanvas(window.innerWidth, window.innerHeight);
-        boardTestInit();
     };
 }
 
@@ -21,15 +19,15 @@ function draw() {
 }
 
 
-function boardTestInit(){
-    //Code for testing board.js
-    board = new Board(width, height, boxSize, boxSize);
-    for (let y = 0; y < board.rows; y++) {
-        for (let x = 0; x < board.cols; x++) {
-            board.setBoardState(x, y, Math.floor(Math.random() * 2));
-        }
-    }
-    board.setStartingPoint(Math.floor(Math.random() * board.cols), Math.floor(Math.random() * board.rows));
-    board.setEndingPoint(Math.floor(Math.random() * board.cols), Math.floor(Math.random() * board.rows));
-    board.changeStateColor(board.STATE_BLOCK, color(100))
-}
+// function boardTestInit(){
+//     //Code for testing board.js
+//     board = new Board(width, height, boxSize, boxSize);
+//     for (let y = 0; y < board.rows; y++) {
+//         for (let x = 0; x < board.cols; x++) {
+//             board.setBoardState(x, y, Math.floor(Math.random() * 2));
+//         }
+//     }
+//     board.setStartingPoint(Math.floor(Math.random() * board.cols), Math.floor(Math.random() * board.rows));
+//     board.setEndingPoint(Math.floor(Math.random() * board.cols), Math.floor(Math.random() * board.rows));
+//     board.changeStateColor(board.STATE_BLOCK, color(100))
+// }
